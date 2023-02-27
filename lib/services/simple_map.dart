1fd6d7837025db3a99169cb3c182a34fb8a60017
spaunkoska22 <sara.paunkoska22@gmail.com>
2023-02-27 20:05:59 +0100

    Location-based reminders added

--- lib/services/simple_map.dart
+++ lib/services/simple_map.dart
@@ -1,14 +1,17 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:examsapp/services/notifications_service.dart';
 
-class SimpleMap extends StatefulWidget{
+class SimpleMap extends StatefulWidget {
   @override
   _SimpleMapState createState() => _SimpleMapState();
 }
 
 class _SimpleMapState extends State<SimpleMap> {
-
   GoogleMapController? mapController; //controller for Google map
   PolylinePoints polylinePoints = PolylinePoints();
 
@@ -17,37 +20,64 @@ class _SimpleMapState extends State<SimpleMap> {
   Set<Marker> markers = Set(); //markers for google map
   Map<PolylineId, Polyline> polylines = {}; //poly-lines to show direction
 
-  LatLng startLocation = LatLng(41.11037902717075, 20.806958533569365); //my home location
-  LatLng endLocation = LatLng(42.00425615556532, 21.409560647534775); //faculty location
+  LatLng startLocation =
+      const LatLng(41.11037902717075, 20.806958533569365); //my home location
+  LatLng endLocation =
+      const LatLng(41.11286580160372, 20.807683609828402); //high school location
 
+  LocalNotificationsService notificationsService = LocalNotificationsService();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  Timer? timer;
+
+  void _checkLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    double distanceInMeters = await Geolocator.distanceBetween(
+        position.latitude,
+        position.longitude,
+        endLocation.latitude,
+        endLocation.longitude);
+    if (distanceInMeters < 500) {
+      notificationsService.showNotification(
+          id: 1,
+          title: 'Entered Target Location',
+          body: 'You have tasks to complete at this location!');
+    }
+  }
 
   @override
   void initState() {
-    markers.add(Marker(
-      markerId: MarkerId(startLocation.toString()),
-      position: startLocation,
-      infoWindow: InfoWindow(
-        title: 'Starting Point ',
-        snippet: 'Start Marker',
+    markers.add(
+      Marker(
+        markerId: MarkerId(startLocation.toString()),
+        position: startLocation,
+        infoWindow: const InfoWindow(
+          title: 'Home',
+          snippet: 'Start Marker',
+        ),
+        icon: BitmapDescriptor.defaultMarker,
       ),
-      icon: BitmapDescriptor.defaultMarker,
-    ),
     );
 
-    markers.add(Marker(
-      markerId: MarkerId(endLocation.toString()),
-      position: endLocation,
-      infoWindow: InfoWindow(
-        title: 'Destination Point ',
-        snippet: 'Destination Marker',
+    markers.add(
+      Marker(
+        markerId: MarkerId(endLocation.toString()),
+        position: endLocation,
+        infoWindow: const InfoWindow(
+          title: 'Faculty',
+          snippet: 'Destination Marker',
+        ),
+        icon: BitmapDescriptor.defaultMarker,
       ),
-      icon: BitmapDescriptor.defaultMarker,
-    ),
     );
 
     getDirections(); //fetch direction poly-lines from Google API
 
     super.initState();
+    Geolocator.getPositionStream().listen((position) {
+      _checkLocation();
+    });
   }
 
   getDirections() async {
@@ -71,7 +101,7 @@ class _SimpleMapState extends State<SimpleMap> {
   }
 
   addPolyLine(List<LatLng> polylineCoordinates) {
-    PolylineId id = PolylineId("poly");
+    PolylineId id = const PolylineId("poly");
     Polyline polyline = Polyline(
       polylineId: id,
       color: Colors.deepPurpleAccent,
@@ -86,7 +116,7 @@ class _SimpleMapState extends State<SimpleMap> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Route Directions in Google Map"),
+        title: const Text("Route Directions in Google Map"),
         backgroundColor: Colors.deepPurpleAccent,
       ),
       body: GoogleMap(
